2e4a6793af6d1f6ccac236f0248f69b40be77db6
Sain Rai <sahinkulung@gmail.com>
2026-07-09 22:16:20 +0545

    day one

---
+++ Dayy1.js
@@ -0,0 +1,15 @@
+// Q1. What's the difference between let, const, and var? Give one example where using const would cause an error
+// let variable bhaneko blocked-scoped ra changeable hunxa const bhanne pani blocked-scoped hunxa tara mutable hudaina testai gari var bhaneko chai function-scoped hunxa ra older version ho future ma bug dina sakxa
+let x = 10;
+x = "ten";
+console.log(x, typeof x);
+
+console.log(typeof null);
+console.log(typeof undefined);
+console.error("NO Smoking");
+
+var a = "hello world";
+let a1 = 12;
+console.log(a);
+console.log(a1);
+

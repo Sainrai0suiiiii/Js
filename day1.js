d8e16fffbd03ffafa4427dbecb363460ec50bbcb
Sain Rai <sahinkulung@gmail.com>
2026-07-09 22:16:10 +0545

    first day

---
+++ day1.js
@@ -0,0 +1,38 @@
+let name = "Anmol";
+console.log("my name is" + name);
+
+const name1 = 'sain';
+console.log(`my name is ${name1}`);
+
+// create variables; age,county,isStudent
+let age,county,isStudent;
+age = 10;
+county = "nepal";
+isStudent = true;
+address = "pathri";
+
+
+console.log(`age:${age}`);
+console.log(`county:${county}`);
+console.log(`yes i am student:${isStudent}`);
+console.log(`this is where i belong:${address}`);
+
+let firstname = 'john';
+let lastname = 'doe';
+
+const fullname = firstname + lastname ;
+console.log(fullname);
+
+//Which variables can change?const pi = 3.14;pi = 5;// What happens?Explain why.
+// variable pi is change able and variable which has be decleard as const pi is not immutable//
+
+let a = 5 ;
+a = 10;
+console.log(a);
+
+const name11 = 'Sain';
+let age11 = '19';
+
+console.log(`My name is ${name11} and i am ${age11} years old`);
+
+name1 = "bhim";

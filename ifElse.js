4ad801f1d7a5319e6da18f16a405749551b50586
Sain Rai <sahinkulung@gmail.com>
2026-07-12 01:28:44 +0545

    elseif

---
+++ ifElse.js
@@ -0,0 +1,35 @@
+//conditional statement allows prgrams to make decisions based on specific conditions'
+// if-else statement
+let age = 25
+if (age>23){
+    console.log("congratulation you are old enough to leave home");
+}
+
+let aim = 50000;
+if ( aim > 500000){
+    console.log("work hard!!!");
+}else{
+    console.log("have a nice tri!!");
+}
+
+//nested if else
+
+let sain = 5.5;
+if (sain == 5.5){
+    if (sain >5.2){
+        console.log("perfect for sain kulung!!");
+    }
+    if ( sain > 5.3){
+        console.log("still perfect!!");
+    }
+    else{
+        console.log("you are unfit bitch!!0000");
+    }
+}
+
+let sandhaya = 20;
+if (sandhaya == 10){
+    console.log("sandhya is 10");
+}
+else if( sandhaya == 15)
+    console.log

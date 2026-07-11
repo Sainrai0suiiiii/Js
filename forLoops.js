ef9c2ac3f4a558e8e92a865f8a5440a676826587
Sain Rai <sahinkulung@gmail.com>
2026-07-12 02:44:56 +0545

    loops

---
+++ forLoops.js
@@ -0,0 +1,31 @@
+//for loop allows code to be executed repeatedly based on condition
+//has three part initialization,condition,increment
+for (let x =0; x <= 7; x++){
+    console.log("Value of x", x);
+}
+
+let x = 234;
+for( i= 1; i <= 10; i++){
+    console.log("Multiple of 234 to 10:",x*i);
+}
+
+let arr = ['sain','sandhya','kulung','lawati'];
+for (i = 0;i<arr.length;i++){
+    console.log("list of mine people:",arr[i]);
+}
+
+//flow chart
+//Initialization counter variable is used to keep track of the number of iterations in the loop and 
+// can initialize multiple counter variables in Statement 1
+
+for (let x = 1; x <=4; x++){
+    console.log("value of x:",x);
+}
+
+// Statement 2: testing condition checks the boolean value of the testing condition
+// if the testing condition is true the loop will execute further otherwise loop will end and the code outside the loop will executed
+
+for(x=3; ;x++){
+    console.log("value of x:" +x);
+    
+}

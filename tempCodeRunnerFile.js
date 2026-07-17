7f8869fd6d578ab3054425040dc34d6b38f51dc3
Sain Rai <sahinkulung@gmail.com>
2026-07-17 12:28:48 +0545

    changes

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+const discountMatch = (match * discount)/100;
+    // const finalDiscount = match - discountMatch
+
+    // return{
+    //     discount,
+    //     discountMatch,
+    //     finalDiscount
+    // };
